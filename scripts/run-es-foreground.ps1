bf14088f90df5b6dcc9fb1d2935ff066338f06f8
ads.kborrero <ads.kborrero@accenture.com>
2026-03-28 03:08:00 +0530

    Updating directory structure

--- scripts/run-es-foreground.ps1
+++
@@ -1,8 +0,0 @@
-# Run Elasticsearch in foreground to observe startup logs
-param(
-    [string]$EsHome = 'C:\dev\elasticsearch-9.1.4'
-)
-Set-Location $EsHome
-Write-Host "Running Elasticsearch in foreground from $EsHome\bin\elasticsearch.bat"
-& "$EsHome\bin\elasticsearch.bat"
-Write-Host "Elasticsearch process exited."
